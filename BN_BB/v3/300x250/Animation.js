fcc3792dfe6c9973bb03b313f12882dbcc74606d
schao87 <schaosti@gmail.com>
2015-10-26 17:06:46 -0400

    logo fades last frame

--- BN_BB/v3/300x250/Animation.js
+++ BN_BB/v3/300x250/Animation.js
@@ -131,13 +131,14 @@ home_animation = (function() {
 		TweenLite.to(black_headphone_left, 1, {left:-300, delay:0 });
 		TweenLite.to(white_headphone, 0.8, {left:0, opacity:1, delay:0.2 });
 
-
+		
 		TweenLite.delayedCall(2.6, frame5);	
 
 	}
 
 	function frame5()
 	{	
+		TweenLite.to(jbl_logos, 0.3, {opacity:0, delay:0});
 		TweenLite.to(for_music_text, 0.3, {opacity:0, delay:0});
 		TweenLite.to(jbl_everest_logo, 0.8, {left:0, opacity:1, delay:0.2 });
 		TweenLite.to(learnMore, 0.8, {left:0, opacity:1, delay:0.5 });
