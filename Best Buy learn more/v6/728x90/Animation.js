9b6cad80e719cc7c1ca5dbb5251de30aaee6f532
schao87 <schaosti@gmail.com>
2015-11-02 17:28:41 -0500

    built 728 bb bn

--- Best Buy learn more/v6/728x90/Animation.js
+++ Best Buy learn more/v6/728x90/Animation.js
@@ -45,7 +45,7 @@ home_animation = (function() {
 
 
 
-		TweenLite.to(black_headphone_blur_right, 0, {left:0});
+		TweenLite.to(black_headphone_blur_right, 0, {left:360});
 
 		frame1();
 
@@ -56,28 +56,29 @@ home_animation = (function() {
 	function frame1()
 	{
 		//headphone slides in
-		TweenLite.to(black_headphone_1, 0.5, {left:0, delay:0});
+		TweenLite.to(black_headphone_1, 0.5, {left:420, delay:0});
 		//wire slides in	
-		TweenLite.to(wire, 0.5, {left:35, delay:0});
+		TweenLite.to(wire, 0.5, {left:450, delay:0});
 		//headphone moves left while rotating
 		//headphones rotate
-		TweenLite.to(black_headphone_1, 1.5, {rotationY:180, delay:1.2, ease: Expo.easeOut});
+		TweenLite.to(black_headphone_1, 1.8, {left:260,rotationY:180, delay:1.2, ease: Expo.easeOut});
 
 		//blur image fades in
-		TweenLite.to(black_headphone_blur_right, .4, {left:-135,opacity:1, delay:1.3, ease: Expo.easeOut});	
-			
+		TweenLite.to(black_headphone_blur_right, 1.9, {left:30,opacity:1, delay:1.3, ease: Expo.easeOut});	
+		
 		//wire falls off
-		TweenLite.to(wire, 4.5, {top:300, left:350, rotationY:180, delay:1.2});	
+		TweenLite.to(wire, 4.5, {top:120, left:350, rotationY:180, delay:1.2});	
 
 		//blur fades out
-		TweenLite.to(black_headphone_blur_right, .7, {opacity:0, delay:1.4, ease: Expo.easeOut});
+		TweenLite.to(black_headphone_blur_right, 1.2, {opacity:0, delay:1.4, ease: Expo.easeOut});
 		//headphones in final position
-		TweenLite.to(black_headphone_left, .2, {opacity:1, delay:1.4});	
-		TweenLite.to(black_headphone_1, .2, {opacity:0, delay:1.4});	
+		TweenLite.to(black_headphone_left, 1, {opacity:1, delay:1.4, ease: Expo.easeOut});	
+		TweenLite.to(black_headphone_left, .8, {left:200, delay:1.4, ease: Expo.easeOut});	
+		TweenLite.to(black_headphone_1, .2, {opacity:0, delay:1.4, ease: Expo.easeOut});	
 
 
 
-		TweenLite.delayedCall(1.5, frame2);
+		TweenLite.delayedCall(2, frame2);
 		
 	}
 	
@@ -106,13 +107,13 @@ home_animation = (function() {
 
     	}
 	
-		//TweenLite.delayedCall(3.5, frame3);	
+		TweenLite.delayedCall(3.5, frame3);	
 
 	}
 	
 	function frame3()
 	{	
-		TweenLite.to(whileless_holder, 0.3, {opacity:0, delay:0 });
+		TweenLite.to(wireless_holder, 0.3, {opacity:0, delay:0 });
 		TweenLite.to(intro_text, 0.3, {opacity:0, delay:0 });
 		TweenLite.to(headphone_text, 0.3, {opacity:0, delay:0 });
 
@@ -134,14 +135,14 @@ home_animation = (function() {
 	function frame5()
 	{	
 
-		TweenLite.to(black_headphone_left, 1, {left:-300, delay:0 });
-		TweenLite.to(white_headphone, 0.8, {left:0, opacity:1, delay:0.2 });
+		TweenLite.to(black_headphone_left, 1, {left:-400, delay:0 });
+		TweenLite.to(white_headphone, 0.8, {left:219, opacity:1, delay:0.3 });
 
 		TweenLite.to(jbl_logos, 0.3, {opacity:0, delay:0});
 		TweenLite.to(for_music_text, 0.3, {opacity:0, delay:0});
-		TweenLite.to(jbl_everest_logo, 0.8, {left:0, opacity:1, delay:0.2 });
-		TweenLite.to(learnMore, 0.8, {left:0, opacity:1, delay:0.5 });
-		TweenLite.to(bestbuy_logo, 0.8, {left:0, opacity:1, delay:0.8 });
+		TweenLite.to(jbl_everest_logo, 0.8, {left:0, opacity:1, delay:0.3 });
+		TweenLite.to(learnMore, 0.8, {opacity:1, delay:0.5 });
+		TweenLite.to(bestbuy_logo, 0.8, {opacity:1, delay:0.8 });
 		TweenLite.to(cta_bottom_rollover, 0.2, {opacity:0});
 		endFrame = 1;
 		
