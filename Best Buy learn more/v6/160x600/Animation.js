ab690802412c7daddfd23a7633182e748017c04c
schao87 <schaosti@gmail.com>
2015-11-02 13:39:02 -0500

    learn more revisions

--- Best Buy learn more/v6/160x600/Animation.js
+++ Best Buy learn more/v6/160x600/Animation.js
@@ -54,23 +54,24 @@ home_animation = (function() {
 	function frame1()
 	{
 		//headphone slides in
-		TweenLite.to(black_headphone_1, 0.5, {left:-90, delay:0});
+		TweenLite.to(black_headphone_1, 0.5, {left:-110, delay:0});
 		//wire slides in	
-		TweenLite.to(wire, 0.5, {left:-61, delay:0});
+		TweenLite.to(wire, 0.5, {left:-81, delay:0});
 		//headphone moves left while rotating
 		//headphones rotate
-		TweenLite.to(black_headphone_1, 1.5, {rotationY:180, delay:1.2, ease: Expo.easeOut});
+		TweenLite.to(black_headphone_1, 1.5, {left:0,rotationY:180, delay:1.2, ease: Expo.easeOut});
 
 		//blur image fades in
-		TweenLite.to(black_headphone_blur_right, .4, {left:-135,opacity:1, delay:1.3, ease: Expo.easeOut});	
+		TweenLite.to(black_headphone_blur_right, .4, {left:-105,opacity:1, delay:1.3, ease: Expo.easeOut});	
 			
 		//wire falls off
 		TweenLite.to(wire, 4.5, {top:300,  delay:1.2});	
-		TweenLite.to(wire, 8, { left:10,  delay:1.2});		
+		TweenLite.to(wire, 4, { left:20,  delay:1.2});		
 
 		//blur fades out
 		TweenLite.to(black_headphone_blur_right, .7, {opacity:0, delay:1.4, ease: Expo.easeOut});
 		//headphones in final position
+		TweenLite.to(black_headphone_left, 0, {left:8, delay:1.4});
 		TweenLite.to(black_headphone_left, .2, {opacity:1, delay:1.4});	
 		TweenLite.to(black_headphone_1, .2, {opacity:0, delay:1.4});	
 
@@ -111,7 +112,7 @@ home_animation = (function() {
 	
 	function frame3()
 	{	
-		TweenLite.to(whileless_holder, 0.3, {opacity:0, delay:0 });
+		TweenLite.to(wireless_holder, 0.3, {opacity:0, delay:0 });
 		TweenLite.to(intro_text, 0.3, {opacity:0, delay:0 });
 		TweenLite.to(headphone_text, 0.3, {opacity:0, delay:0 });
 
@@ -134,7 +135,7 @@ home_animation = (function() {
 	{	
 
 		TweenLite.to(black_headphone_left, 1, {left:-300, delay:0 });
-		TweenLite.to(white_headphone, 0.8, {left:0, opacity:1, delay:0.2 });
+		TweenLite.to(white_headphone, 0.8, {left:4, opacity:1, delay:0.2 });
 
 		TweenLite.to(jbl_logos, 0.3, {opacity:0, delay:0});
 		TweenLite.to(for_music_text, 0.3, {opacity:0, delay:0});
